ac3fcb21c99cc7d94cc8503693939396174dd40d
Karla Thamiris <karlatn@outlook.com>
2020-11-02 15:38:50 -0300

    Array e dicionário

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var frutas = [{nome:"maçã", cor:"vermelho"}, {nome:"goiaba", cor:"roséo"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/* Dicionário
+var fruta = {nome:"maçã", cor:"vermelho"};
+console.log(fruta.nome);
+*/
+
+
+//var lista = ["maçã", "goiaba", "pêra"];
+//console.log(lista);
+
+
+
+//lista.push("uva"); // Adicionar elemento a lista, entrará no final
+//lista.pop(); Para retirar um elemento da lista
+
+
+//console.log(lista.reverse); Imprimir os elementos da lista do último ao primeiro
+//console.log(lista.toString()) // Transforma em string
+//console.log(lista.join(" - ")) // Transforma em string e ainda separa da forma como colocada entre as aspas
+
+
+//alert(lista[2]);
+
+
+//var nome = "Karla";
+//var idade = "25";
+//alert(nome + " atualmente tem " + idade + " anos");
